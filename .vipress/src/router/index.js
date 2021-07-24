093b8c0338a21618033f097352991048db837b37
ektx <530675800@qq.com>
2021-07-24 19:11:32 +0800

    feat: 测试新功能

--- .vipress/src/router/index.js
+++ .vipress/src/router/index.js
@@ -1,5 +1,5 @@
 // ‼️ 注意 此文件自动创建，请务手动修改内容
-// 创建时间: Tue Jul 20 2021 19:51:33 GMT+0800 (中国标准时间)
+// 创建时间: Sat Jul 24 2021 18:45:06 GMT+0800 (中国标准时间)
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../../md-pages/README/index.vue";
@@ -13,9 +13,9 @@ export default createRouter({
       component: Home,
     },
     {
-      path: "/docs/build",
-      name: "view-docs-build",
-      component: () => import("../../md-pages/docs/build/index.vue"),
+      path: "/README",
+      name: "view-README",
+      component: () => import("../../md-pages/README/index.vue"),
     },
     {
       path: "/docs/doc",
@@ -33,19 +33,14 @@ export default createRouter({
       component: () => import("../../md-pages/docs/install/index.vue"),
     },
     {
-      path: "/docs/readme",
-      name: "view-docs-readme",
-      component: () => import("../../md-pages/docs/readme/index.vue"),
-    },
-    {
       path: "/docs/setting",
       name: "view-docs-setting",
       component: () => import("../../md-pages/docs/setting/index.vue"),
     },
     {
-      path: "/README",
-      name: "view-README",
-      component: () => import("../../md-pages/README/index.vue"),
+      path: "/docs/test",
+      name: "view-docs-test",
+      component: () => import("../../md-pages/docs/test/index.vue"),
     },
   ],
 });
